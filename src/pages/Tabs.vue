dc837ffa2d875cca226cb7fbf05e1e5f90f2a3c0
霍微信 <wahid@girtu.com>
2017-12-21 11:41:03 +0800

    修改编码，支持中文

--- src/pages/Tabs.vue
+++ src/pages/Tabs.vue
@@ -1,5 +1,6 @@
-<template>
-  <div>
+<template class="content">
+  <div class="content">
+    <div  class="bar bar-nav">
     <v-toolbar dark color="primary">
       <v-toolbar-side-icon></v-toolbar-side-icon>
       <v-toolbar-title class="white--text">Title</v-toolbar-title>
@@ -10,9 +11,24 @@
       <v-btn icon><v-icon>refresh</v-icon></v-btn>
       <v-btn icon><v-icon>more_vert</v-icon></v-btn>
     </v-toolbar>
+    </div>
 
     <h1>tabs</h1>
     <a @click="toMarkdown()">走</a>
+
+    <footer></footer>
+    <footer></footer>
+    <footer></footer>
+    <footer></footer>
+    <footer></footer>
+    <footer></footer>
+    <footer></footer>
+    <footer></footer>
+    <footer></footer>
+    <footer></footer>
+    <footer></footer>
+    <footer></footer>
+    <footer></footer>
   </div>
 </template>
 
@@ -29,4 +45,37 @@
     }
   }
 </script>
+
+<style>
+  .content {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    overflow: auto;
+    -webkit-overflow-scrolling: touch;
+  }
+  .bar {
+    position: absolute;
+    right: 0;
+    left: 0;
+    z-index: 10;
+    height: 2.2rem;
+    padding-right: 0.5rem;
+    padding-left: 0.5rem;
+    background-color: #f7f7f8;
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;  }
+  .bar-nav {
+    top: 0;
+  }
+
+  footer {
+    background-color: gray;
+    height: 100px;
+    padding: 10px;
+    margin: 10px;
+  }
+</style>
 
